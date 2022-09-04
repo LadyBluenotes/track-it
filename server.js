fd68f3ca2a2c7e96869656c2b14dad19df863b4c
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-09-04 11:05:16 -0700

    fixed error that wasn't starting app

--- server.js
+++ server.js
@@ -8,12 +8,11 @@ const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
-const pathRoutes = require('./routes/paths')
 
 require('dotenv').config({path: './config/.env'})
 
-//Passport config
-require('.config/passport')(passport)
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -22,26 +21,24 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(logger('dev'))
-
 // Sessions
 app.use(
     session({
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: false,
-        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
     })
-)
-
+  )
+  
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash())
-
+  
 app.use('/', mainRoutes)
-app.use('/paths', pathRoutes)
-
+ 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}, you better catch it!`)
+    console.log(`Server is running`)
 })    
