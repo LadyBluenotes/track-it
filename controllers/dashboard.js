9ff042c1ba8de9304c2666da6370a0386361250b
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-09-12 10:07:05 -0700

    fixed error where dashboard was not rendering

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,5 +3,10 @@ const Dashboard = require('../models/Dashboard')
 module.exports = {
     getDashboard: async (req, res) => {
         console.log(req.user)
+        try {
+            res.render('dashboard.ejs')
+        } catch(err){
+            console.log(err)
+        }
     }
 }
